9dd03b806cb9b86db033f0f6ee39105ce77f62aa
thnewlands <thomas@newlands.cc>
2015-08-22 14:17:59 -0700

    Herewego

---
+++ Unity-ArmsandLegs/Arms n Legs/Assets/armBender.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class armBender : MonoBehaviour {
+    public float torque;
+	
+	public float turnHor = 0;
+	public float turnVer = 0;
+
+    void FixedUpdate() {
+		
+		float turnHor = Input.GetAxis("Horizontal");
+		float turnVer = Input.GetAxis("Vertical");
+        transform.Rotate(Vector3.right * turnHor * torque);
+		transform.Rotate(Vector3.back * turnVer * torque);
+    }
+}
---
+++ Unity-ArmsandLegs/Arms n Legs/Assets/armBenderUpgrade.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+ using System.Collections;
+ 
+ public class armBenderUpgrade : MonoBehaviour {
+     
+     //Rotation Sensitivity
+     public float RotationSensitivity = 35.0f;
+     public float minAngle = -45.0f;
+     public float maxAngle = 45.0f;
+     
+     //Rotation Value
+     float yRotate = 0.0f;
+     float zRotate = 0.0f;
+	 
+     // Update is called once per frame
+     void Update () {
+         
+         //Rotate X view
+         yRotate += Input.GetAxis ("Horizontal") * RotationSensitivity * Time.deltaTime;
+         yRotate = Mathf.Clamp (yRotate, minAngle, maxAngle);
+         transform.eulerAngles = new Vector3 (yRotate, 0.0f, 0.0f);
+		 
+		 //Rotate Z view
+         zRotate += Input.GetAxis ("Vertical") * RotationSensitivity * Time.deltaTime;
+         zRotate = Mathf.Clamp (zRotate, minAngle, maxAngle);
+         transform.eulerAngles = new Vector3 (0.0f, 0.0f, zRotate);
+     }
+ }
+ //http://answers.unity3d.com/questions/762475/limit-range-camera-rotation.html
