f0dc4a18e0028a26574daff4f14272937911bb30
thnewlands <thomas@newlands.cc>
2015-08-23 14:49:58 -0700

    sticky!!
    but wont push itself

---
+++ Unity-ArmsandLegs/Arms n Legs/Assets/Scripts/sticky.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class sticky : MonoBehaviour {
+
+public bool jointCollided = false;
+public GameObject rigidBodysource;
+
+void OnTriggerEnter(Collider other) {
+		  if(other.GetComponent<Collider>().tag == "Stickable")
+			 {
+				print("Stickable");
+				rigidBodysource.GetComponent<Rigidbody>().isKinematic = true;
+				jointCollided = true; // stop physics
+			 }
+	}
+}
+
+//transform.parent = other.transform; // doesn't move yet, but will move w/what it hit
