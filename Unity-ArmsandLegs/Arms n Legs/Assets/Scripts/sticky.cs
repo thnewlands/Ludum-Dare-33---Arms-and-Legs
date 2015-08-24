8559518e55c086bbd05c0780a4cdf845934b1ce3
thnewlands <thomas@newlands.cc>
2015-08-23 17:41:57 -0700

    working detach (press space)

--- Unity-ArmsandLegs/Arms n Legs/Assets/Scripts/sticky.cs
+++ Unity-ArmsandLegs/Arms n Legs/Assets/Scripts/sticky.cs
@@ -3,18 +3,31 @@ using System.Collections;
 
 public class sticky : MonoBehaviour {
 
-public bool jointCollided = false;
 public GameObject rigidBodysource;
 public GameObject rootTransform;
+public bool jointCollided = false;
+public float gracePeriodTimer = 0f;
 
 void OnTriggerEnter(Collider other) {
-		  if(other.GetComponent<Collider>().tag == "Stickable")
-			 {
+	if(gracePeriodTimer < Time.time){
+		if(other.GetComponent<Collider>().tag == "Stickable"){
 				print("Stickable");
 				Destroy (rigidBodysource.GetComponent<Rigidbody>());
 				rootTransform.transform.parent = other.transform;
 				jointCollided = true;
-			 }
+		}
 	}
 }
 
+void Update() {
+	if (jointCollided = true){
+		if (Input.GetKeyDown("space")){
+			rigidBodysource.AddComponent<Rigidbody>();
+			rootTransform.transform.parent = rootTransform.transform.parent;
+			jointCollided = false;
+			gracePeriodTimer = Time.time + 3f;
+		}
+	}
+}
+
+}
