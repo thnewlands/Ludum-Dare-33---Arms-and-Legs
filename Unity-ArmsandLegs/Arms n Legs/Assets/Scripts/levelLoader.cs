f070ffff82ffdfa075716a89a73a5236dcbd435e
thnewlands <thomas@newlands.cc>
2015-08-24 17:14:30 -0700

    added intro screen and bench works now!

---
+++ Unity-ArmsandLegs/Arms n Legs/Assets/Scripts/levelLoader.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class levelLoader : MonoBehaviour {
+
+	void Update () {
+		if(Input.GetButton("Jump")){
+			Application.LoadLevel (1);
+		}
+	}
+}
+
